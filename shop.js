ee0de25ea1edbc4f10993c7206118b7f2739d844
dtaipov <dtaipov@gmail.com>
2017-01-08 19:53:27 +0400

    cache-control removed. Menu style changed

--- shop.js
+++ shop.js
@@ -49,14 +49,6 @@ console.log('Connecting to database...');
     app.use(express.static(__dirname + '/shop/public'));
   
 //});
-
-app.get('/*', function (req, res, next) {
-    if (req.url.indexOf("/img/style/") != -1) {
-        console.log("HELLO: "  + req.url);
-        res.setHeader("Cache-Control", "public, max-age=2592000");
-    }
-    next();
-});
     
 // Require router, passing passport for authenticating pages
 require('./shop/router')(app, passport);
