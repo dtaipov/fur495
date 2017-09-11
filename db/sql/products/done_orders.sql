b4c967e9aa3838bc8f520b5c49f46e6397a201b3
dtaipov <dtaipov@gmail.com>
2017-09-11 11:56:01 +0300

    show_to_public field introduced to product table

--- db/sql/products/done_orders.sql
+++ db/sql/products/done_orders.sql
@@ -1,4 +1,5 @@
 select
+    fo.document_id,
     to_char(d.creation, 'YYYY-MM-DD') creation,
     fo.money_amount,
     fo.product_id,
@@ -9,7 +10,7 @@ select
     p.product_group_id,
     itp.image_url,
     pg.name group_name
- from 
+ from
  finance_operation fo, document d, product_group pg, product p
  left join
  image_to_product itp
@@ -17,6 +18,7 @@ select
 where
  fo.document_id = d.id and
  fo.product_id = p.id and
+ p.show_to_public = true and
  pg.id = p.product_group_id and
  d.active = TRUE and
  d.document_type_id = 4
