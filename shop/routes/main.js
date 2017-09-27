f67cdb6cd6e00e06455730fa96c34ef6b8d3d1c9
Dmitry Taipov <dtaipov@gmail.com>
2017-09-27 15:07:52 +0300

    i18n. Page titles and Contacts page translated

--- shop/routes/main.js
+++ shop/routes/main.js
@@ -3,7 +3,7 @@ const db = require('../data');
 module.exports = {
   getHome: (req, res) => {
     res.render('main/home', {
-      title: "Fur_Atelier",
+      title: req.__("Fur_Atelier"),
       logged: req.isAuthenticated(),
       user: req.user
     });
@@ -11,7 +11,7 @@ module.exports = {
 
   getAbout: (req, res) => {
     res.render('main/about', {
-      title: "Menu_About_Us",
+      title: req.__("Menu_About_Us"),
       logged: req.isAuthenticated(),
       user: req.user,
     });
@@ -19,7 +19,7 @@ module.exports = {
 
   getServices: (req, res) => {
     res.render('main/services', {
-      title: "Menu_Services",
+      title: req.__("Menu_Services"),
       logged: req.isAuthenticated(),
       user: req.user,
     });
@@ -28,7 +28,7 @@ module.exports = {
   getDoneOrders: (req, res) => {
     db.getDoneOrders((err, doneOrders) => {
       res.render('main/done_orders', {
-        title: "Menu_Done_Orders",
+        title: req.__("Menu_Done_Orders"),
         logged: req.isAuthenticated(),
         doneOrders,
         user: req.user
@@ -38,7 +38,7 @@ module.exports = {
 
   getEShops: (req, res) => {
     res.render('main/e_shops', {
-      title: "Menu_Online_Shops",
+      title: req.__("Menu_Online_Shops"),
       logged: req.isAuthenticated(),
       user: req.user
     });
@@ -46,7 +46,7 @@ module.exports = {
 
   getContact: (req, res) => {
     res.render('main/contact', {
-      title: "Menu_Contacts",
+      title: req.__("Menu_Contacts"),
       logged: req.isAuthenticated(),
       user: req.user,
     });
