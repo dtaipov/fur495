07a3f8d770b48b67145487d1a30337f0bd97f48b
Dmitry Taipov <dtaipov@gmail.com>
2017-09-27 14:35:43 +0300

    i18n. Page titles translation

--- shop/routes/main.js
+++ shop/routes/main.js
@@ -1,105 +1,54 @@
-// Require needed modules
-var db = require('../data'),
-config = require('../config.json');
+const db = require('../data');
 
-// Export functions
 module.exports = {
-  // Get shop home page
-  getHome: function(req, res) {
-    // Get categories for top nav
-    db.getTopCategories(function(err, categories) {
-      if (err) {console.log(err)}
-
-      // Get featured products
-      db.getFeatured(function(err, featured) {
-        if (err) {console.log(err)}
-
-        // Render home page
-        res.render('main/home', {
-          store: config.store.name,
-          title: config.store.tagline,
-          logged: req.isAuthenticated(),
-          user: req.user,
-          //cart: req.session.cart,
-          categories: categories,
-          featured: featured
-        });
-      });
+  getHome: (req, res) => {
+    res.render('main/home', {
+      title: "Fur_Atelier",
+      logged: req.isAuthenticated(),
+      user: req.user
     });
   },
 
-  // Get about page
-  getAbout: function(req, res) {
-
-    // Get categories for top nav
-    db.getTopCategories(function(err, categories) {
-      if (err) {console.log(err)}
-
-      // Render contact page
-      res.render('main/about', {
-        store: config.store.name,
-        title: 'О нас',
-        logged: req.isAuthenticated(),
-        user: req.user,
-        //cart: req.session.cart,
-        categories: categories,
-      });
+  getAbout: (req, res) => {
+    res.render('main/about', {
+      title: "Menu_About_Us",
+      logged: req.isAuthenticated(),
+      user: req.user,
     });
   },
 
-  getServices: function(req, res) {
-
-    db.getTopCategories(function(err, categories) {
-      if (err) {console.log(err)}
-
-      res.render('main/services', {
-        store: config.store.name,
-        title: 'Услуги',
-        logged: req.isAuthenticated(),
-        user: req.user,
-        //cart: req.session.cart,
-        categories: categories
-      });
+  getServices: (req, res) => {
+    res.render('main/services', {
+      title: "Menu_Services",
+      logged: req.isAuthenticated(),
+      user: req.user,
     });
   },
 
   getDoneOrders: (req, res) => {
     db.getDoneOrders((err, doneOrders) => {
       res.render('main/done_orders', {
-        store: config.store.name,
-        title: 'Выполненные услуги',
+        title: "Menu_Done_Orders",
         logged: req.isAuthenticated(),
-        doneOrders: doneOrders,
+        doneOrders,
         user: req.user
       });
     });
   },
 
-  getEShops: function(req, res) {
+  getEShops: (req, res) => {
     res.render('main/e_shops', {
-      store: config.store.name,
-      title: 'Интернет-магазины',
+      title: "Menu_Online_Shops",
       logged: req.isAuthenticated(),
       user: req.user
     });
   },
 
-  // Get contact page
-  getContact: function(req, res) {
-
-    // Get categories for top nav
-    db.getTopCategories(function(err, categories) {
-      if (err) {console.log(err)}
-
-      // Render contact page
-      res.render('main/contact', {
-        store: config.store.name,
-        title: 'Контакты',
-        logged: req.isAuthenticated(),
-        user: req.user,
-        //cart: req.session.cart,
-        categories: categories,
-      });
+  getContact: (req, res) => {
+    res.render('main/contact', {
+      title: "Menu_Contacts",
+      logged: req.isAuthenticated(),
+      user: req.user,
     });
   }
 };
