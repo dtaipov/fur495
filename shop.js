af168c3b37cb59979bb9694efe028071ce3d00d8
Dmitry Taipov <dtaipov@gmail.com>
2017-09-27 11:22:21 +0300

    i18n introduced

--- shop.js
+++ shop.js
@@ -1,53 +1,38 @@
-// Require express
 const express = require('express');
-// Set up express
 const app = express();
-// Require mongostore session storage
-//var mongoStore = require('connect-mongo')(express);
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-// Require needed files
-//const database = require('./shop/data');
+const i18n = require('i18n');
 const config = require('./shop/config.json');
 const info = require('./package.json');
 
-console.log('NodeShop Started!');
-
-// Connect to database
-//database.startup(config.connection);
-console.log('Connecting to database...');
+i18n.configure({
+  locales: ['en', 'ru'],
+  defaultLocale: 'en',
+  cookie: 'smm_locale',
+  directory: __dirname + '/locales'
+});
   
 // Configure Express
 //app.configure(function(){
-    
-    // Set up jade
-    app.set('views', __dirname + '/shop/views');
-    app.set('view engine', 'pug');
-    
-    //app.use(express.favicon());
-    const cookieParserWithSecrets = cookieParser('novanova');
-    app.use(cookieParserWithSecrets);
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
-    app.locals.env = process.env;
 
-    // Set up sessions
-    /*app.use(express.session({
-        // Set up MongoDB session storage
-        store: new mongoStore({url:config.connection}),
-        // Set session to expire after 21 days
-        cookie: { maxAge: new Date(Date.now() + 181440000)},
-        // Get session secret from config file
-        secret: config.cookie_secret
-        }));
-    
-    // Set up passport*/
-    app.use(passport.initialize());
-    app.use(passport.session());
-    
-    // Define public assets
-    app.use(express.static(__dirname + '/shop/public'));
+app.set('views', __dirname + '/shop/views');
+app.set('view engine', 'pug');
+
+//app.use(express.favicon());
+const cookieParserWithSecrets = cookieParser('novanova');
+app.use(cookieParserWithSecrets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.locals.env = process.env;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define public assets
+app.use(express.static(__dirname + '/shop/public'));
+app.use(i18n.init);
   
 //});
     
